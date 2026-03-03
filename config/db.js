051849c333c89442ffdce5b61fd4175abac12d9d
KANISHKA1503 <kanishkasennimalai37@gmail.com>
2026-03-03 23:04:31 +0530

    update MongoDB connection to use environment variable for URI

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 async function connectDB(){
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/vegetable_price');
+        await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/vegetable_price');
         console.log("MongoDB connected Successfully 💥🪄🎊🎉");
     } catch(err)
     {
